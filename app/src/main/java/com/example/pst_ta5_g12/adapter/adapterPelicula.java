e8bc2c1b504815ffd2493b116c0ab2fb68ee7a3d
Marcelo <btigrero@espol.edu.ec>
2021-08-08 12:25:30 -0500

    edicion del layout home

--- app/src/main/java/com/example/pst_ta5_g12/adapter/adapterPelicula.java
+++ app/src/main/java/com/example/pst_ta5_g12/adapter/adapterPelicula.java
@@ -4,6 +4,8 @@ package com.example.pst_ta5_g12.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -22,22 +24,31 @@ public class adapterPelicula extends RecyclerView.Adapter<adapterPelicula.viewho
     @Override
     public viewholderpelicula onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_home,parent,false);
-        return null;
+        viewholderpelicula holder = new viewholderpelicula(v);
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull viewholderpelicula holder, int position) {
-
+        Pelicula pel=peliculaList.get(position);
+        holder.tvpelicula.setText(pel.getAutor());
+        holder.tvautor.setText(pel.getAutor());
+        holder.tveditorial.setText(pel.getEditorial());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return peliculaList.size();
     }
 
     public class viewholderpelicula extends RecyclerView.ViewHolder {
+        TextView tvpelicula,tvautor,tveditorial;
+
         public viewholderpelicula(@NonNull View itemView) {
             super(itemView);
+            tvpelicula = itemView.findViewById(R.id.tvpelicula);
+            tvautor = itemView.findViewById(R.id.tvautor);
+            tveditorial = itemView.findViewById(R.id.tveditorial);
         }
     }
 }
